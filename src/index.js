f8810934c4a1c4c7c0d462b9d1a110e0063872fa
PatienceCrixix1z12 <nutsugahamapatience12@gmail.com>
2024-11-29 12:19:43 +0000

    add all files

---
+++ src/index.js
@@ -0,0 +1,35 @@
+let name =("Patience Nutsugah");
+console.log(name);
+let age =(24 +"years old");
+console.log(age);
+let x=10
+let y=12
+let result =(x+y);
+console.log(result);
+
+let num2=13
+let num3=2
+let total=(num2-num3);
+console.log(total);
+
+let a=70
+let b=20
+let ans=(80/20);
+console.log(ans);
+
+let p=3
+let q=8
+let final=(p*q);
+console.log(final)
+
+let f=15%4
+console.log(f);
+
+let game =true;
+console.log(typeof{game});
+var number =prompt("How many apples do you have?");
+console.log(number +("apples"));
+let fruit =prompt("what is you favourite fruit");
+console.log(fruit);
+
+
